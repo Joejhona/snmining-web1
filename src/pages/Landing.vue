7f1236749e261806bc0ce8bda8498828289b7fcf
joejhona <joejhona@gmail.com>
2019-10-15 11:53:27 -0500

    segundo dia

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -49,12 +49,11 @@
               >
                 <!-- First image on the left side -->
                 <p class="blockquote blockquote-info">
-                  "Over the span of the satellite record, Arctic sea ice has
-                  been declining significantly, while sea ice in the
-                  Antarctichas increased very slightly"
+                  "A nivel latinoamericano, la minería peruana ocupa el primer 
+                  puesto en producción de oro, zinc, estaño y plomo. A nivel mundial, 
+                  ocupa el segundo lugar en producción de plata y cobre."
                   <br />
-                  <br />
-                  <small>-NOAA</small>
+                  <small>-OSINERGMIN</small>
                 </p>
               </div>
               <!-- Second image on the left side of the article -->
@@ -70,34 +69,22 @@
                 style="background-image: url('img/bg1.jpg')"
               ></div>
               <h3>
-                So what does the new record for the lowest level of winter ice
-                actually mean
+                La Misión, Visión y Valores de SN Mining
               </h3>
               <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
+                Nuestra <strong>Misión</strong> es brindar productos y servicios 
+                mineros con estándares de calidad para poder satisfacer y superar 
+                las necesidades y expectativas de nuestros clientes.
               </p>
               <p>
-                For a start, it does not automatically follow that a record
-                amount of ice will melt this summer. More important for
-                determining the size of the annual thaw is the state of the
-                weather as the midnight sun approaches and temperatures rise.
-                But over the more than 30 years of satellite records, scientists
-                have observed a clear pattern of decline, decade-by-decade.
+                Tenemos la <strong>Visión</strong> de ser reconocidos a nivel local
+                y regional como lideres en la fabricación, desarrollo e implementación 
+                de equipos y servicios mineros.
               </p>
               <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
+                Nuestros <strong>Valores</strong>: La pasión por un buen trabajo, 
+                La satisfacción del cliente, La apertura a la mejora continua, La innovación
+                en nuestros procesos y la Calidad personal y su integridad. 
               </p>
             </div>
           </div>
@@ -106,59 +93,135 @@
     </div>
     <div class="section section-team text-center">
       <div class="container">
-        <h2 class="title">Here is our team</h2>
+        <h2 class="title">Nuestro equipo</h2>
         <div class="team">
           <div class="row">
             <div class="col-md-4">
               <div class="team-player">
                 <img
-                  src="img/avatar.jpg"
+                  src="img/avatar1.jpg"
                   alt="Thumbnail Image"
                   class="rounded-circle img-fluid img-raised"
                 />
-                <h4 class="title">Romina Hadid</h4>
-                <p class="category text-info">Model</p>
+                <h4 class="title">Felix Flores</h4>
+                <p class="category text-info">Gerente General</p>
                 <p class="description">
-                  You can write here details about one of your team members. You
-                  can give more details about what they do. Feel free to add
-                  some <a href="#">links</a> for people to be able to follow
-                  them outside the site.
+                  Ing. Mecanico con mas de 20 años de experiencia en Minería 
                 </p>
-                <a href="#pablo" class="btn btn-info btn-icon btn-round"
-                  ><i class="fab fa-twitter"></i
+                <!--<a href="https://www.linkedin.com/in/tomas-aguirre-quispe-30724637/" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-linkedin"></i
+                ></a>-->
+                <a href="https://wa.me/51993526844?text=Me%20gustaría%20saber%20mas%20de%20SN%20Mining" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-whatsapp"></i
                 ></a>
-                <a href="#pablo" class="btn btn-info btn-icon btn-round"
-                  ><i class="fab fa-instagram"></i
-                ></a>
-                <a href="#pablo" class="btn btn-info btn-icon btn-round"
-                  ><i class="fab fa-facebook-square"></i
-                ></a>
+                <a class="btn btn-info btn-icon btn-round" 
+                  v-b-popover.hover.top="" title="felix.flores@snmining.com"
+                  ><i style="color:white;" class="fab fa-telegram-plane"></i
+                >
+                </a>
               </div>
             </div>
             <div class="col-md-4">
               <div class="team-player">
                 <img
-                  src="img/ryan.jpg"
+                  src="img/avatar3.jpg"
                   alt="Thumbnail Image"
                   class="rounded-circle img-fluid img-raised"
                 />
-                <h4 class="title">Ryan Tompson</h4>
-                <p class="category text-info">Designer</p>
+                <h4 class="title">Tomas Aguirre</h4>
+                <p class="category text-info">Proyectos & Ingeniería</p>
                 <p class="description">
-                  You can write here details about one of your team members. You
-                  can give more details about what they do. Feel free to add
-                  some <a href="#">links</a> for people to be able to follow
-                  them outside the site.
+                  Ing. Mecanico con mas de 20 años de experiencia en Minería 
                 </p>
-                <a href="#pablo" class="btn btn-info btn-icon btn-round"
-                  ><i class="fab fa-twitter"></i
-                ></a>
-                <a href="#pablo" class="btn btn-info btn-icon btn-round"
+                <!--<a href="https://www.linkedin.com/in/tomas-aguirre-quispe-30724637/" class="btn btn-info btn-icon btn-round"
                   ><i class="fab fa-linkedin"></i
+                ></a>-->
+                <a href="https://wa.me/51993526844?text=Me%20gustaría%20saber%20mas%20de%20SN%20Mining" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-whatsapp"></i
                 ></a>
+                <a class="btn btn-info btn-icon btn-round" 
+                  v-b-popover.hover.top="" title="tomas.aguirre@snmining.com"
+                  ><i style="color:white;" class="fab fa-telegram-plane"></i
+                >
+                </a>
               </div>
             </div>
             <div class="col-md-4">
+              <div class="team-player">
+                <img
+                  src="img/avatar5.jpg"
+                  alt="Thumbnail Image"
+                  class="rounded-circle img-fluid img-raised"
+                />
+                <h4 class="title">Joel Ticse</h4>
+                <p class="category text-info">Energía Renovable</p>
+                <p class="description">
+                  Especialista en Energía Renovable con mas de 10 años de experiencia. 
+                </p>
+                <!--<a href="https://www.linkedin.com/in/tomas-aguirre-quispe-30724637/" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-linkedin"></i
+                ></a>-->
+                <a href="https://wa.me/51914854941?text=Me%20gustaría%20saber%20mas%20de%20SN%20Mining" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-whatsapp"></i
+                ></a>
+                <a class="btn btn-info btn-icon btn-round" 
+                  v-b-popover.hover.top="" title="joel.ticse@snmining.com"
+                  ><i style="color:white;" class="fab fa-telegram-plane"></i
+                >
+                </a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="team-player">
+                <img
+                  src="img/avatar2.jpg"
+                  alt="Thumbnail Image"
+                  class="rounded-circle img-fluid img-raised"
+                />
+                <h4 class="title">Jassuco Balbin</h4>
+                <p class="category text-info">Administración & Finanzas</p>
+                <!--<p class="description">
+                  Especialista en Energia Renovable con mas de 10 años de experiencia 
+                </p>-->
+                <!--<a href="https://www.linkedin.com/in/tomas-aguirre-quispe-30724637/" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-linkedin"></i
+                ></a>-->
+                <a href="https://wa.me/51936918717?text=Me%20gustaría%20saber%20mas%20de%20SN%20Mining" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-whatsapp"></i
+                ></a>
+                <a class="btn btn-info btn-icon btn-round" 
+                  v-b-popover.hover.top="" title="jassuco.balbin@snmining.com"
+                  ><i style="color:white;" class="fab fa-telegram-plane"></i
+                >
+                </a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="team-player">
+                <img
+                  src="img/avatar4.jpg"
+                  alt="Thumbnail Image"
+                  class="rounded-circle img-fluid img-raised"
+                />
+                <h4 class="title">Victor Ponce</h4>
+                <p class="category text-info">Administración & Finanzas</p>
+                <!--<p class="description">
+                  Especialista en Energia Renovable con mas de 10 años de experiencia 
+                </p>-->
+                <!--<a href="https://www.linkedin.com/in/tomas-aguirre-quispe-30724637/" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-linkedin"></i
+                ></a>-->
+                <a href="https://wa.me/51941693803?text=Me%20gustaría%20saber%20mas%20de%20SN%20Mining" class="btn btn-info btn-icon btn-round"
+                  ><i class="fab fa-whatsapp"></i
+                ></a>
+                <a class="btn btn-info btn-icon btn-round" 
+                  v-b-popover.hover.top="" title="victor.ponce@snmining.com"
+                  ><i style="color:white;" class="fab fa-telegram-plane"></i
+                >
+                </a>
+              </div>
+            </div>
+            <!--<div class="col-md-4">
               <div class="team-player">
                 <img
                   src="img/eva.jpg"
@@ -183,11 +246,12 @@
                   ><i class="fab fa-twitter"></i
                 ></a>
               </div>
-            </div>
+            </div>-->
           </div>
         </div>
       </div>
     </div>
+    <!--
     <div class="section section-contact-us text-center">
       <div class="container">
         <h2 class="title">Want to work with us?</h2>
@@ -227,6 +291,7 @@
         </div>
       </div>
     </div>
+    -->
   </div>
 </template>
 <script>
